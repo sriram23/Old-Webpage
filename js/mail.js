3cb97695bce91cb539c75fbc6cbf1cb95bbde7a5
sriram23 <sriramsubramanian23@gmail.com>
2019-09-15 14:47:54 +0530

    Initial Commit

---
+++ js/mail.js
@@ -0,0 +1,36 @@
+ $('form#sendBtn button.submit').click(function() {
+		alert("Hello World");
+       /*$('#image-loader').fadeIn();
+
+       var contactName = $('#contactForm #name').val();
+       var contactEmail = $('#contactForm #email1').val();
+       var contactMessage = $('#contactForm #message1').val();
+
+       var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail + '&contactMessage=' + contactMessage;
+
+       $.ajax({
+
+ 	      type: "POST",
+ 	      url: "inc/sendEmail.php",
+ 	      data: data,
+ 	      success: function(msg) {
+
+             // Message was sent
+             if (msg == 'OK') {
+                $('#image-loader').fadeOut();
+                $('#message-warning').hide();
+                $('#contactForm').fadeOut();
+                $('#message-success').show();
+             }
+             // There was an error
+             else {
+                $('#image-loader').fadeOut();
+                $('#message-warning').html(msg);
+ 	            $('#message-warning').fadeIn();
+             }
+
+ 	      }
+
+       });
+       return false;*/
+    });
